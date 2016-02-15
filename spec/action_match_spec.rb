da9450bfaad073555749d400af568f2f1b410903
Rachel Evans <git@rve.org.uk>
2016-02-15 13:55:58 +0000

    Splitting tests

---
+++ spec/action_match_spec.rb
@@ -0,0 +1,9 @@
+require_relative "../lib/aws_policy_simulator"
+
+describe "match on Action / NotAction" do
+
+  it "blah" do
+    true
+  end
+
+end
---
+++ spec/condition_match_spec.rb
@@ -0,0 +1,9 @@
+require_relative "../lib/aws_policy_simulator"
+
+describe "match on Conditions" do
+
+  it "blah" do
+    true
+  end
+
+end
